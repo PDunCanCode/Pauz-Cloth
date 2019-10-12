65025874d090fcb408d9c7dc3c64197d32d2fd3a
Paul D. Duncan <chesiredarling@gmail.com>
2019-10-12 15:56:37 -0700

    FB DB stuffz

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,17 +3,18 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCR6YymPBobqL5mu1MUpS95RVssAEMp-e8",
-    authDomain: "pauz-db.firebaseapp.com",
-    databaseURL: "https://pauz-db.firebaseio.com",
-    projectId: "pauz-db",
-    storageBucket: "",
-    messagingSenderId: "246061100968",
-    appId: "1:246061100968:web:4561eaf2cf69868c3bb79d",
-    measurementId: "G-CYCNBDWNGN"
+  apiKey: "AIzaSyCR6YymPBobqL5mu1MUpS95RVssAEMp-e8",
+  authDomain: "pauz-db.firebaseapp.com",
+  databaseURL: "https://pauz-db.firebaseio.com",
+  projectId: "pauz-db",
+  storageBucket: "pauz-db.appspot.com",
+  messagingSenderId: "246061100968",
+  appId: "1:246061100968:web:4561eaf2cf69868c3bb79d",
+  measurementId: "G-CYCNBDWNGN"
   };
 
   firebase.initializeApp(config);
+  firebase.analytics();
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
